b841a5d2a44e6a6f5bbfb132677f856b0262a656
Mizanurmd <mizanur021991@gmail.com>
2023-06-24 09:14:00 +0600

    TaskController-Home.Html

--- src/main/java/ToDoAppProject/controller/EmailController.java
+++ src/main/java/ToDoAppProject/controller/EmailController.java
@@ -1,12 +1,15 @@
 package ToDoAppProject.controller;
 
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ToDoAppProject.dto.EmailSender;
 import ToDoAppProject.model.Task;
@@ -16,7 +19,8 @@ import ToDoAppProject.service.TaskService;
 
 @Controller
 public class EmailController {
-
+	@Autowired
+	private VelocityEngine ve;
 	@Autowired
 	private TaskService taskServ;
 	@Autowired
@@ -32,9 +36,12 @@ public class EmailController {
 	}
 	
 	@GetMapping("/test")
+	
 	public String testvelocity() {
+
+		return "vtemplates/test.vm";
 		
-		return "templates/vtemplates/class.vm";
+		
 	}
 
 }
--- src/main/java/ToDoAppProject/controller/TaskController.java
+++ src/main/java/ToDoAppProject/controller/TaskController.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import ToDoAppProject.model.Task;
+
+
 import ToDoAppProject.service.TaskService;
 
 @Controller
